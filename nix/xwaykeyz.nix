a0a61cf84ef7d5b711b17b9ab09eafb4ec765001
Celes Renata <celes@frameshift.net>
2026-05-01 05:33:59 +0000

    Fix: relax inotify-simple version constraint in xwaykeyz (1.3 -> >=1.3)

--- nix/xwaykeyz.nix
+++ nix/xwaykeyz.nix
@@ -49,7 +49,8 @@ buildPythonPackage rec {
   postPatch = ''
     substituteInPlace pyproject.toml \
       --replace-fail '"dbus-python ~= 1.3.2"' '"dbus-python >= 1.3.2"' \
-      --replace-fail '"hyprpy ~= 0.1.5"' '"hyprpy >= 0.1.5"'
+      --replace-fail '"hyprpy ~= 0.1.5"' '"hyprpy >= 0.1.5"' \
+      --replace-fail '"inotify-simple ~= 1.3"' '"inotify-simple >= 1.3"'
   '';
 
   propagatedBuildInputs = [
