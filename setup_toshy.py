8d48f30a544abe3a8134aacb9005f7298c529e45
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2026-06-11 18:30:04 -0800

    Add files to ignore patterns list (no copying)

--- setup_toshy.py
+++ setup_toshy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = '20260602'                        # CLI option "--version" will print this out.
+__version__ = '20260611'                        # CLI option "--version" will print this out.
 
 import os
 os.environ['PYTHONDONTWRITEBYTECODE'] = '1'     # prevent this script from creating cache files
@@ -3807,8 +3807,10 @@ def install_toshy_files():
                 'LICENSE',
                 'packages.json',
                 'prep_centos_before_setup.sh',
+                'pyrightconfig.json',
                 'README.md',
                 'requirements.txt',
+                'ruff.toml',
                 this_file_name,
         ]
         # must use list unpacking (*) ignore_patterns() requires individual pattern arguments
