c5c553d2d509f67d26e213bdc0793250bd492a7a
celesrenata <celesrenata@users.noreply.github.com>
2026-06-27 18:04:06 -0700

    fix: relax python-xlib version constraint for xwaykeyz

--- nix/xwaykeyz.nix
+++ nix/xwaykeyz.nix
@@ -50,7 +50,8 @@ buildPythonPackage rec {
     substituteInPlace pyproject.toml \
       --replace-fail '"dbus-python ~= 1.3.2"' '"dbus-python >= 1.3.2"' \
       --replace-fail '"hyprpy ~= 0.1.5"' '"hyprpy >= 0.1.5"' \
-      --replace-fail '"inotify_simple ~= 1.3"' '"inotify_simple >= 1.3"'
+      --replace-fail '"inotify_simple ~= 1.3"' '"inotify_simple >= 1.3"' \
+      --replace-fail '"python-xlib == 0.31"' '"python-xlib >= 0.31"'
   '';
 
   propagatedBuildInputs = [
