3081cec5fd1ab30b6629c042152c1f5a1cef4373
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2026-07-15 02:05:16 -0800

    Bump module versions

--- toshy_gui/main_tkinter.py
+++ toshy_gui/main_tkinter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__version__ = '20250717'
+__version__ = '20260715'
 
 # Preferences app for Toshy, using tkinter GUI and "Sun Valley" theme
 TOSHY_PART      = 'gui'   # CUSTOMIZE TO SPECIFIC TOSHY COMPONENT! (gui, tray, config)
